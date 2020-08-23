0961176f62469e2be28a2f7d39eeaf19a47e8fe6
WisnuArya <wisnu.arya.tegar.anggaraksa@gmail.com>
2020-08-23 21:19:09 +0700

    add Shared Preferences

---
+++ app/src/main/java/com/example/wisnu11rpl12019/MainMenu.java
@@ -0,0 +1,15 @@
+
+package com.example.wisnu11rpl12019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
